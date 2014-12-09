641a4e49eee1496d17faae00b292c609ef497a6e
James <jmaclennan@gmail.com>
2014-12-08 22:35:28 -0800

    fixed broken wget in install script

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ cp .vimrc ~/.vimrc
 mkdir -p ~/.vim/autoload
 
 echo "** installing pathogen"
-wget ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "** creating plugin dirs"
 mkdir ~/.vim/bundle
 mkdir ~/.vim/colors
 cd ~/.vim/bundle
 
-echo "**installing plugins"
+echo "** installing plugins"
 git clone https://github.com/scrooloose/nerdtree
 git clone https://github.com/tpope/vim-fugitive
 git clone https://github.com/bling/vim-airline
